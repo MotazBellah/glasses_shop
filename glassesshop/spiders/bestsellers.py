7e78de5339962a724792d90e1052c4c2ac062d7f
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-09-30 13:22:26 +0200

    Scrap glasses shop website and get  'product url', 'product image link', 'product name' and the 'product price'

--- glassesshop/spiders/bestsellers.py
+++ glassesshop/spiders/bestsellers.py
@@ -7,4 +7,19 @@ class BestsellersSpider(scrapy.Spider):
     start_urls = ['https://www.glassesshop.com/bestsellers']
 
     def parse(self, response):
-        pass
+        products_list = response.xpath("//div[@id='product-lists']/div")
+
+        for i in products_list:
+            link = i.xpath(".//div[2]/a/@href").get()
+            glass_image = i.xpath(".//div[2]/a/img[1]/@src").get()
+            tryon_image = i.xpath(".//div[2]/a/img[2]/@src").get()
+            name = i.xpath(".//div[3]/div[2]/div[1]/div[1]/div[1]/a/@title").get()
+            price = i.xpath(".//div[3]/div[2]/div[1]/div[2]/div[1]/div[1]/span/text()").get()
+
+            yield {
+                'name': name,
+                'price': price,
+                'link': link,
+                'glass_image': glass_image,
+                'try_on_image': tryon_image,
+            }
