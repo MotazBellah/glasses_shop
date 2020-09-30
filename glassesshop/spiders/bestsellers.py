527707c727343e7adaf0a46fde5a30069a8635c7
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-09-30 12:43:07 +0200

    Startproject and generate spider to scrap glasses shop website

---
+++ glassesshop/spiders/bestsellers.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class BestsellersSpider(scrapy.Spider):
+    name = 'bestsellers'
+    allowed_domains = ['www.glassesshop.com']
+    start_urls = ['https://www.glassesshop.com/bestsellers']
+
+    def parse(self, response):
+        pass
